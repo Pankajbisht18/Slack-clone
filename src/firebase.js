2eeba78b389b89159ded981e260342b3ecc96c78
Pankajbisht18 <techpankaj45@gmail.com>
2022-10-30 23:22:06 +0530

    Create header section

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBHszvEvhvlVsM59AwBdIdiTUzYrU0YeL8",
+    authDomain: "slack-clone-c6fd1.firebaseapp.com",
+    projectId: "slack-clone-c6fd1",
+    storageBucket: "slack-clone-c6fd1.appspot.com",
+    messagingSenderId: "806928361812",
+    appId: "1:806928361812:web:860ed92ce35a59531a00c1"
+};
